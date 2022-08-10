9fc6b5a39daf1e4a939f1129a7cd85be8359466c
franciscobaeck <franciscobaeck@hotmail.com>
2022-08-09 23:11:56 -0300

    arreglos y objetos

--- js/main.js
+++ js/main.js
@@ -1,46 +1,51 @@
+let interes = 0;
+const carrito = [];
 
+function calcularPrecioFinal(importeSinInteres, cantCuotas) {
+  let precioFinal;
+  if (cantCuotas == 1) {
+    interes = 0;
+    precioFinal = importeSinInteres;         
+  } else if (cantCuotas == 3) {
+    interes = 30;
+    precioFinal = importeSinInteres * ((interes+100)/100);        
+  } else {
+    interes = 40;
+    precioFinal = importeSinInteres * ((interes+100)/100);
+  }
 
-let importeSinInteres = parseInt(prompt("ingrese el importe sin interés"));
-let interes=0;
-
-
-console.log("importe sin interes", importeSinInteres)
-while (isNaN(importeSinInteres)==true || importeSinInteres == null || importeSinInteres == "" ){
-  importeSinInteres = prompt("ingresar un número y no un caracter como importe sin interés");
+  return parseFloat(precioFinal).toFixed(2);
 }
-console.log("importe sin interes", importeSinInteres)
 
-  let cantCuotas = parseInt((prompt("ingrese cantidad de cuotas")));
+const form = document.getElementsByClassName('needs-validation')[0]
 
-  while (cantCuotas !== 1 && cantCuotas !== 3 && cantCuotas !== 6) {
-    if (isNaN(cantCuotas)==true) {
-      cantCuotas = parseInt(prompt("ingrese un número y no un caracter para cantidad de cuotas"));  
+form.addEventListener('submit', event => {
+  event.preventDefault()
+  event.stopPropagation()
+
+  form.classList.add('was-validated')
+
+  if (form.checkValidity()) {
+    const nombre = document.getElementById('nomyape').value
+    const producto = document.getElementById('producto').value
+    const importe = document.getElementById('importe').value
+    const cuotas = document.getElementById('cuotas').value
+    const precioFinal = calcularPrecioFinal(importe, cuotas)
+
+    const compra = {
+      id: carrito.length + 1,
+      nombre,
+      producto,
+      importe,
+      cuotas,
+      precioFinal,
+      interes
     }
-    else {
-      cantCuotas = parseInt(prompt("No disponemos de esa cantidad de cuotas, ingrese nuevamente"));
-    }
+
+    carrito.push(compra)
+    
+    alert(`Según el método de pago elegido, el importe final es de $${precioFinal} a pagar en ${cuotas} pago/s. El interés es de ${interes}%. El valor de la cuota es $${(precioFinal/cuotas).toFixed(2)}`)
+
   }
-
-  function PrecioFinal (importeSinInteres,cantCuotas){
-
-    if (cantCuotas == 1) {
-      interes=0;
-      return importeSinInteres;      
-            
-    } else if (cantCuotas == 3) {
-      interes=30;
-      return importeSinInteres * ((interes+100)/100);        
-    } else {
-      interes=40;
-      return importeSinInteres * ((interes+100)/100);
-    }  
-  }
-
-  console.log(PrecioFinal(importeSinInteres,cantCuotas))
-
-  alert("Según el método de pago elegido, el importe final es de $"+ PrecioFinal (importeSinInteres,cantCuotas)+" a pagar en "+ cantCuotas+" pago/s. El interés es de "+ interes+"%. El valor de la cuota es $"+ PrecioFinal (importeSinInteres,cantCuotas)/cantCuotas)
-
-  const compra = {interes: interes, cuotas:cantCuotas, total:PrecioFinal(importeSinInteres,cantCuotas)};
-
-  //Guardo la compra en un objeto
-  console.log(compra)
+}, false)
+console.log(carrito)
